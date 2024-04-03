b99f699edb3e164190aad372cd17e03438705bc9
Maria Eduarda <102489310+MEdudaCampos@users.noreply.github.com>
2024-04-03 12:39:15 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+const App = () => {
+    return (
+        <main className='container-main'>
+            <div className='box-principal'>
+                <div className='infos'>
+                <img src="../img/img-perfil.png" alt="" />
+            <h2>Maria Eduarda</h2>
+            <h3>Maceió/AL, Brasil</h3>
+            <h4>Estudante Senai no curso de Técnico em desenvolvimento pra web</h4>
+                </div>
+            <div className='container-options'>
+                    <a href="#">GitHub</a>
+                    <a href="#">Frontend Mentor</a>
+                    <a href="#">LinkedlIn</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Instagram</a>
+            </div>
+                    
+            </div>
+
+        </main>
+    )
+}
+
+export default App
